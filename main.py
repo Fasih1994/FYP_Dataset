073ca52d705731a31edeadc86a99373be72c57cb
Fasih1994 <fasihuddin1994@gmail.com>
2019-11-04 22:55:27 +0500

    adding train

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ example_img = cv2.imread(example_img_path)
 example_img = cv2.resize(src=example_img, dsize=(height, width), interpolation=cv2.INTER_NEAREST)
 
 base_path = 'data'
-name = 'test2'
+name = 'train'
 base_path = os.path.join(base_path, name)
 if not os.path.exists(base_path):
     os.mkdir(base_path)
@@ -23,7 +23,7 @@ while capture.isOpened():
     ret, frame = capture.read()
     if ret is True:
         cv2.imshow('Reference Image', example_img)
-        cv2.rectangle(frame, start_point, end_point, (255, 255, 64), thickness=3)
+        cv2.rectangle(frame, start_point, end_point, (255, 255, 64), thickness=1)
         cv2.imshow('Video Frame', frame)
         key = cv2.waitKey(1)
         if key != -1:
@@ -33,7 +33,7 @@ while capture.isOpened():
                 img_path = os.path.join(base_path, key)
                 if not os.path.exists(img_path):
                     os.makedirs(img_path)
-                image = frame[start_point[1] + 3:end_point[1], start_point[0] + 3:end_point[0]]
+                image = frame[start_point[1] + 1:end_point[1], start_point[0] + 1:end_point[0]]
                 filename = '{}/{}.png'.format(img_path, len(os.listdir(img_path)))
                 cv2.imwrite(filename=filename, img=image)
                 print('image saved at ', filename)
